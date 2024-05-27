8f4c928ec681c4058108d8f7cad6eface3b49d45
leenacr7 <144852848+leenacr7@users.noreply.github.com>
2024-05-28 02:52:16 +0300

    Update and rename script.js to scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+// Initialize Bootstrap carousel
+$(document).ready(function() {
+    $('.carousel').carousel({
+        interval: 2000 // Change slide every 2 seconds
+    });
+});
+
+// Custom JavaScript functionality can be added here
+// For example, form validation, dynamic content loading, etc.
+
